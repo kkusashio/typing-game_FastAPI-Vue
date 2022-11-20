ad21a26cf339f96a6adb3c45c3c2bd19ef2914b4
Junm0ri <yukkurimorijun@gmail.com>
2022-11-20 16:23:28 +0900

    正規表現を削除

--- BackEnd/api/schemas/word.py
+++ BackEnd/api/schemas/word.py
@@ -4,14 +4,14 @@ from pydantic import BaseModel, Field
 
 class Word(BaseModel):
     id: int
-    English_word: str = Field(..., regex=r"^[a-zA-Z]+$", example="hello", description="英単語")
-    Japanese_word: str = Field(..., regex=r"^[ぁ-んァ-ン一-龥]+$", example="こんにちは", description="日本語訳")
-    level: int = Field(..., ge=1, le=5, example=1, description="英単語のレベル")
+    English_word: str = Field(...,  example="hello", description="英単語")
+    Japanese_word: str = Field(...,example="こんにちは", description="日本語訳")
+    level: int = Field(..., ge=1, example=1, description="英単語のレベル")
 
 class WordCreate(BaseModel):
-    English_word: str = Field(..., regex=r"^[a-zA-Z]+$", example="hello", description="英単語")
-    Japanese_word: str = Field(..., regex=r"^[ぁ-んァ-ン一-龥]+$", example="こんにちは", description="日本語訳")
-    level: int = Field(..., ge=1, le=5, example=1, description="英単語のレベル")
+    English_word: str = Field(..., example="hello", description="英単語")
+    Japanese_word: str = Field(..., example="こんにちは", description="日本語訳")
+    level: int = Field(..., ge=1, example=1, description="英単語のレベル")
 
 class WordCreateResponse(WordCreate):
     id: int
