7b2946fa46de4a3a11bf90698acf6907c6e642c2
yskfukui <1403yfukui@gmail.com>
2022-12-01 01:03:08 +0900

    FIX:環境変数の読み込みを修正

--- BackEnd/api/config.py
+++ BackEnd/api/config.py
@@ -2,4 +2,4 @@ from starlette.config import Config
 from starlette.datastructures import Secret
 
 config = Config(".env")
-SECRET_KEY = config("SECRET_KEY", cast=Secret)
+SECRET_KEY = config("SECRET_KEY")
