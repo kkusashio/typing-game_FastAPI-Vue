05c16ac94c05fce47e2ec985529ed33bfadb4e4e
kkusashio <kkusashio@gmail.com>
2022-12-03 15:46:23 +0900

    frontend env setups

---
+++ front-end/Dockerfile
@@ -0,0 +1,3 @@
+FROM node:14.17.1
+WORKDIR /src
+RUN yarn global add @vue/cli
---
+++ front-end/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    hello
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style>
+
+</style>
