ac1d4361cd0bbf2a0fe8d67714ccdd9a37fc6ec9
kkusashio <kkusashio@gmail.com>
2022-12-09 18:47:08 +0900

    routeing and pages initialization

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <hello-world />
+  <div>
+    <h1>welcome</h1>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
 
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'HomeView',
-
+export default {
+  name: "HomeView",
   components: {
-    HelloWorld,
-  },
-});
+  }
+};
 </script>
+
+<style>
+</style>
