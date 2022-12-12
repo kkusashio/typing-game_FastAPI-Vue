cc2262416017e5d41c2a838c63e011f8e4816528
kkusashio <kkusashio@gmail.com>
2022-12-12 17:21:58 +0900

    page routing and components check

--- front-end/src/components/HeaderBar.vue
+++ front-end/src/components/HeaderBar.vue
@@ -1,29 +1,28 @@
 <template>
-  <div>
-      <v-app-bar 
-      color="surface" 
-      height="100" 
-      class="el" 
-      app 
-      :style="{ padding : $vuetify.breakpoint.mdAndUp ? '0px 100px': '' }"
-      >
-      <v-toolbar-title 
-      @click="$router.push('/')" 
-      class="text-md-h5 font-weight-bold pointer">
-      <img src="/logo.png"  width="100vh">
-      </v-toolbar-title>
-      
-      <v-spacer/>
-      <v-btn nuxt to="/projects" icon>
-          <v-icon size="20">mdi-application-brackets</v-icon>
-      </v-btn>
-      <v-btn nuxt to="/members" icon>
-          <v-icon size="20">mdi-account-group</v-icon>
-      </v-btn>
-      <v-divider vertical class="mx-md-5 mx-2"/>
-      <v-btn nuxt to="/" icon>
-          <v-icon size="20">mdi-home</v-icon>
-      </v-btn>
-      </v-app-bar>
-  </div>
+  <v-app-bar app color="#aaaaaa" dark flat class="px-8">
+    <v-btn icon>
+        <v-icon color="#e1f223">fa-user</v-icon>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon size="20">mdi-application-brackets</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon size="20">mdi-account-group</v-icon>
+    </v-btn>
+    <v-divider vertical class="mx-md-5 mx-2"/>
+    <v-btn to="/about" icon>
+      <v-icon size="20">mdi-home</v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
+
+<script lang="ts">
+export default {
+  methods:{}
+  }
+</script>
+
+<style>
+
+</style>
--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <h1>welcome</h1>
+    <Navbar />
   </div>
 </template>
 
 <script lang="ts">
+import Navbar from '../components/HeaderBar.vue'
 
 export default {
   name: "HomeView",
   components: {
+    Navbar,
   }
 };
 </script>
