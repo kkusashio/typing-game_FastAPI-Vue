3782a9f0d4853ef76c29ac18515fa861553b450b
yskfukui <1403yfukui@gmail.com>
2022-12-24 01:42:12 +0900

    WIP:動きません

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <v-app>
+  <v-app>
     <v-main>
       <h1 class="display-2 font-weight-black ml-10 mt-10">
         Welcome to Vuetify 3 Beta
@@ -7,19 +7,22 @@
       <v-container>
         <v-row class="ma-8">
           <v-col v-for="[title] in links"  :key="title" cols="4">
-            <v-card height="200" class="ma-8">
+            <v-card height="150" class="ma-8" @click="toType">
               <v-card-title class="text-center">{{title}}</v-card-title>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
     </v-main>
-  </v-app> -->
+  </v-app>
   <TypingApp></TypingApp>
 </template>
 
 <script lang="ts">
 import TypingApp from '../components/TypingApp.vue'
+import { useRouter } from 'vue-router'
+const router = useRouter();
+console.log(router)
 export default {
   name: "HomeView",
   data(){
@@ -33,6 +36,11 @@ export default {
   },
   components: {
     TypingApp
+  },
+  methods: {
+    toType: function () {
+      router.push("/user")
+    }
   }
 };
 </script>
